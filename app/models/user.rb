1a019e8a32fd209b248edd7925d36f430732f35e
Stanford Rosenthal <nolastan@gmail.com>
2013-02-01 17:43:37 -0800

    timing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :day, :habits, :hour, :token, :uid
+  attr_accessible :day, :habits, :hour, :token, :uid, :delayed_job_id
   before_save :schedule
 
   def schedule
@@ -7,11 +7,15 @@ class User < ActiveRecord::Base
     Delayed::Job.find(self.delayed_job_id).destroy if self.delayed_job_id
 
     # next whatever day of week
-    date = Date.today
+    Time.zone = "Pacific Time (US & Canada)"
+    date = DateTime.now.midnight
     date += ((day - date.wday) % 7)
 
+    # time of day
+    date = date.advance(:hours => hour, :minutes => 0)
+
     # enqueue job
-    dj = Delayed::Job.enqueue(TaskPost.new, 0, date)
+    dj = TaskPost.delay(:run_at => date).new(:user => self)
 
     # save id of delayed job on radar record
     self.delayed_job_id = dj.id
